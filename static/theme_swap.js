098e0cec87800a5fd50742cb40fa6667042dfdf9
999cmp <999cmp@mail.ru>
2024-12-25 21:49:49 +0300

    update

---
+++ static/theme_swap.js
@@ -0,0 +1,11 @@
+document.getElementById('toggle-theme').addEventListener('click', () => {
+  const body = document.body;
+  body.classList.toggle('new-year');
+
+  // Анимация для плавного переключения
+  if (body.classList.contains('new-year')) {
+    body.style.transition = 'background 1s ease';
+  } else {
+    body.style.transition = 'background 1s ease';
+  }
+});
