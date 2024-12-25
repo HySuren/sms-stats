7033dc35ebcb3a7a74ec65fd7ad3ddaa88a42f40
999cmp <999cmp@mail.ru>
2024-12-25 22:23:42 +0300

    update

--- static/theme_swap.js
+++ static/theme_swap.js
@@ -1,16 +1,11 @@
-document.getElementById('toggle-theme').addEventListener('click', function () {
+document.getElementById('toggle-theme').addEventListener('click', () => {
   const body = document.body;
+  body.classList.toggle('new-year');
 
-  // Проверяем, активна ли новогодняя тема
+  // Анимация для плавного переключения
   if (body.classList.contains('new-year')) {
-    // Убираем новогоднюю тему
-    body.classList.remove('new-year');
-    body.style.background = 'linear-gradient(180deg, #000, #2f2f2f, #595959 30%)';
+    body.style.transition = 'background 1s ease';
   } else {
-    // Активируем новогоднюю тему
-    body.classList.add('new-year');
-    body.style.background = 'url(/static/snow.png), linear-gradient(180deg, #8b0000, #440000, #300000)';
-    body.style.backgroundSize = 'cover'; // Чтобы изображение занимало весь экран
-    body.style.backgroundAttachment = 'fixed'; // Закрепляем фон
+    body.style.transition = 'background 1s ease';
   }
 });
