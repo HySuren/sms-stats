8760bf4cec3274eccfd7750f209ac6794f70201b
999cmp <999cmp@mail.ru>
2024-12-25 22:20:25 +0300

    update

--- static/theme_swap.js
+++ static/theme_swap.js
@@ -1,11 +1,16 @@
-document.getElementById('toggle-theme').addEventListener('click', () => {
+document.getElementById('toggle-theme').addEventListener('click', function () {
   const body = document.body;
-  body.classList.toggle('new-year');
 
-  // Анимация для плавного переключения
+  // Проверяем, активна ли новогодняя тема
   if (body.classList.contains('new-year')) {
-    body.style.transition = 'background 1s ease';
+    // Убираем новогоднюю тему
+    body.classList.remove('new-year');
+    body.style.background = 'linear-gradient(180deg, #000, #2f2f2f, #595959 30%)';
   } else {
-    body.style.transition = 'background 1s ease';
+    // Активируем новогоднюю тему
+    body.classList.add('new-year');
+    body.style.background = 'url(/static/snow.png), linear-gradient(180deg, #8b0000, #440000, #300000)';
+    body.style.backgroundSize = 'cover'; // Чтобы изображение занимало весь экран
+    body.style.backgroundAttachment = 'fixed'; // Закрепляем фон
   }
 });
